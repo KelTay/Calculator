1964c4ad44fb1ffc6b78f744598ec599ab9fd45e
kelvin <klvno.tay@gmail.com>
2019-05-20 20:22:20 -0700

    Add some click event listeners

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 
+const display = document.querySelector("#div-display");
+const buttons = Array.from(document.querySelectorAll("button"));
+
+buttons.forEach(function (button) {
+
+    // Check that the text content is a number
+    if (!isNaN(button.textContent)) {
+        button.addEventListener("click", function () {
+            display.textContent += button.textContent;
+        });
+    } else {
+
+        switch (button.textContent) {
+            case "C":
+                display.textContent = "";
+            case "Backspace":
+                
+            case "&divide;":
+            case "&times;":
+            case "&minus;":
+            case "+":
+            case "=":
+            case ".":
+            default: console.log("Error in switch statement.");
+        }
+    }
+});
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,6 +43,6 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate (operator, num1, num2) {
+function operate(operator, num1, num2) {
 
 }
