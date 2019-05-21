79b719e90be7f40b4be38fa1733da3db196a202f
kelvin <klvno.tay@gmail.com>
2019-05-21 11:38:46 -0700

    Implement Clear and Backspace buttons

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ buttons.forEach(function (button) {
 
     // Check that the text content is a number
     if (!isNaN(button.textContent)) {
-        button.addEventListener("click", function () {
-            display.textContent += button.textContent;
+
+        button.addEventListener("click", () => {
+            display.textContent += button.textContent
         });
+
     } else {
 
         switch (button.textContent) {
             case "C":
-                
+                button.addEventListener("click", clearDisplay);
             case "Backspace":
-                
+                button.addEventListener("click", backspace);
             case "&divide;":
             case "&times;":
             case "&minus;":
@@ -24,8 +26,11 @@ buttons.forEach(function (button) {
             case ".":
             default: console.log("Error in switch statement.");
         }
+
     }
 });
+
+// Functions for performing basic arithmetic operations
 
 function add(num1, num2) {
     return num1 + num2;
@@ -43,6 +48,20 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+//
 function operate(operator, num1, num2) {
 
+}
+
+// Clear the calculator's display
+function clearDisplay() {
+    display.textContent = "0";
+}
+
+// Remove the last inputted number from the display
+function backspace() {
+    const digit = display.textContent.split("");
+    digit.splice(-1, 1);
+
+    display.textContent = digit.join("");
 }
